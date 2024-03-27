bdca2750146590f23e9d64b689f354587f81382a
Babalola Ibukun <sibukun05@gmail.com>
2024-03-27 15:46:40 -0700

    add

--- src/component/Education.jsx
+++ src/component/Education.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
+import img from '../assets/p1 (2).jpg'
+import img2 from '../assets/p1 (1).png'
 
 const Education = () => {
     return (
         <>
-            
+
             <section data-aos="flip-down" className="aos-init aos-animate">
                 <br /><br />
                 <div id="education" className="flex lg:ms-96 ms-20 gap-5 mb-10" data-aos="flip-down">
-                    
+
                     <div className="text-white text-4xl text-center lg:ms-36">Education</div>
                     <div className="text-white mt-1">
                         <svg stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 24 24" height="35" width="35" xmlns="http://www.w3.org/2000/svg">
@@ -17,13 +19,14 @@ const Education = () => {
                 </div>
                 <div className="text-gray-600 lg:flex my-5 text-center">
                     <div id="edu" className="mx-auto rounded bg-white">
-                        {/* <img className="img-fluid mx-auto lg:mt-5 mt-5 mb-2" width="50" src="/image/sqi.png" alt="" /> */}
+                        <img className="img-fluid mx-auto lg:mt-5 mt-5 mb-2" width="50" src={img} alt="" />
+                        <img src="" alt="" />
                         <h1 className="font-bold">SQI College Of ICT</h1>
                         <p className="font-bold">FULL-STACK Development</p>
                         <p className="font-bold">April 2023 - Feb 2024</p>
                     </div>
                     <div id="edu" className="mx-auto rounded bg-white">
-                        {/* <img className="img-fluid mx-auto lg:mt-5 mt-5 mb-2" width="50" src="/image/lautech.png" alt="" /> */}
+                        <img className="img-fluid mx-auto lg:mt-5 mt-5 mb-2" width="70" src={img2} alt="" />
                         <h1 className="font-bold">UNILORIN</h1>
                         <p className="font-bold">BSc PLANT BIOLOGY</p>
                         <p className="font-bold">2012 - 2017</p>
